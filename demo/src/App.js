7eb759b52cb2e3439616e7f29195b77188fadecb
dangloc6789 <83728109+dangloc6789@users.noreply.github.com>
2022-05-04 21:31:40 +0700

    3

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,11 +1,30 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import { db } from "./firebase-config";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs ,addDoc,updateDoc,doc,deleteDoc} from "firebase/firestore";
 
 function App() {
+  const [newName, setNewName] = useState("")
+  const [newAge, setNewAge] = useState(0)
+
   const [users, setUsers] = useState([]);
   const usersCollectionRef = collection(db, "users");
+
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, {name:newName,age:Number(newAge)});
+  };
+
+  const updateUser = async(id, age) => {
+    const userDoc=doc(db,"users",id)
+    const newFields={age: age+1}
+    await updateDoc(userDoc,newFields)
+  }
+
+  const deleteUser=async(id) => {
+    const userDoc=doc(db,"users",id);
+    await deleteDoc(userDoc);
+  }
+
   useEffect(() => {
 
     const getUsers = async () => {
@@ -18,10 +37,29 @@ function App() {
 
   return (
     <div className="App">
+      <input 
+        style={{ height: "30px" }} 
+        placeholder="Name..." 
+        onChange={(event) => { 
+          setNewName(event.target.value); 
+          }} 
+      />
+      <input 
+        style={{ height: "30px" }} 
+        type="number" 
+        placeholder="Age..." 
+        onChange={(event) => { 
+          setNewAge(event.target.value); 
+          }} 
+      />
+
+      <button style={{ height: "40px" }} onClick={createUser}>Create User</button>
       {users.map((user) => {
         return <div>
-          <h1>Name:{user.name}</h1>
-          <h1>Age:{user.age}</h1>
+          <h1>Tên:{user.name}</h1>
+          <h1>Tuổi:{user.age}</h1>
+          <button style={{ height: "40px" }} onClick={() => {updateUser(user.id,user.age)}}>Tăng tuổi</button>
+          <button style={{ height: "40px" }} onClick={() => {deleteUser(user.id)}}>Xóa người dùng</button>
         </div>;
       })}
     </div>);
